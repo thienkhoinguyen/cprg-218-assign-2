66b129df7f59ecc38bf3dcf0ba949fa60077113f
Khoi <147691737+thienkhoinguyen@users.noreply.github.com>
2024-03-21 20:18:04 -0600

    edited alt text

---
+++ java.js
@@ -0,0 +1,9 @@
+document.getElementById('currentYear').innerHTML = new Date().getFullYear();
+function myFunction() {
+var x = document.getElementById("myLinks");
+if (x.style.display === "block") {
+    x.style.display = "none";
+} else {
+    x.style.display = "block";
+}
+}
